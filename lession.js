0ac94d4faa3940223b9be2659730859153c16754
James Epstein <je451@live.mdx.ac.uk>
2021-10-28 18:07:12 +0100

    First commit

---
+++ lession.js
@@ -0,0 +1,46 @@
+let lessons =
+    [
+        {
+            id: 1,
+            title: "Math",
+            description: "London",
+            price: 100,
+            Stock: 5,
+            image: "math-icon.jpg"
+        },
+
+        {
+            id: 2,
+            title: "Engilsh",
+            description: "London",
+            price: 100,
+            Stock: 5,
+            image: "math-icon.jpg"
+        },
+
+        {
+            id: 3,
+            title: "Math",
+            description: "Oxford",
+            price: 100,
+            Stock: 5,
+            image: "math-icon.jpg"
+        },
+
+        {
+            id: 4,
+            title: "Engilsh",
+            description: "York",
+            price: 80,
+            Stock: 5,
+            image: "math-icon.jpg"
+        },
+        {
+            id: 5,
+            title: "Music",
+            description: "Brisol",
+            price: 90,
+            Stock: 5,
+            image: "math-icon.jpg"
+        }
+]
