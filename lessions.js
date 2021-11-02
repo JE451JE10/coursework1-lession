2981bd5e0a920c607a29fa2013a8a9d43f86bb13
James Epstein <je451@live.mdx.ac.uk>
2021-11-02 17:01:02 +0000

    second commit

---
+++ lessions.js
@@ -0,0 +1,89 @@
+let lessons =
+    [
+        {
+            id: 1,
+            Subject: "Math",
+            Localtion: "London",
+            price: 100,
+            Stock: 5,
+            image: "math icon.png"
+        },
+
+        {
+            id: 2,
+            Subject: "Engilsh",
+            Localtion: "London",
+            price: 100,
+            Stock: 5,
+            image: "English icon.png"
+        },
+
+        {
+            id: 3,
+            Subject: "Science",
+            Localtion: "Oxford",
+            price: 100,
+            Stock: 5,
+            image: "science icon.png"
+        },
+
+        {
+            id: 4,
+            Subject: "Histoy",
+            Localtion: "York",
+            price: 80,
+            Stock: 5,
+            image: "history icon.png"
+        },
+        {
+            id: 5,
+            Subject: "Music",
+            Localtion: "Brisol",
+            price: 90,
+            Stock: 5,
+            image: "Music icon.png"
+        },
+        {
+            id: 6,
+            Subject: "Math",
+            Localtion: "Manchester",
+            price: 150,
+            Stock: 5,
+            image: "math icon.png"
+        },
+
+        {
+            id: 7,
+            Subject: "Engilsh",
+            Localtion: "Southampton",
+            price: 120,
+            Stock: 5,
+            image: "English icon.png"
+        },
+
+        {
+            id: 8,
+            Subject: "Science",
+            Localtion: "Glasgow",
+            price: 50,
+            Stock: 5,
+            image: "science icon.png"
+        },
+
+        {
+            id: 9,
+            Subject: "Histoy",
+            Localtion: "Newcastle ",
+            price: 140,
+            Stock: 5,
+            image: "history icon.png"
+        },
+        {
+            id: 10,
+            Subject: "Music",
+            Localtion: "Plymouth",
+            price: 160,
+            Stock: 5,
+            image: "Music icon.png"
+        }
+]
