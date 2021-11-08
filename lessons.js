f5ee631a484c3e409890db3d34514d565bf951f3
JE451JE10 <93095744+JE451JE10@users.noreply.github.com>
2021-11-08 16:41:52 +0000

    Update lessons.js

--- lessons.js
+++ lessons.js
@@ -1,89 +1,83 @@
-let lessons =
-    [
-        {
-            id: 1,
-            Subject: "Math",
-            Localtion: "London",
-            price: 100,
-            Stock: 5,
-            image: "math icon.png"
-        },
 
+   let lessons = [
         {
-            id: 2,
-            Subject: "Engilsh",
-            Localtion: "London",
-            price: 100,
-            Stock: 5,
-            image: "English icon.png"
-        },
-
-        {
-            id: 3,
-            Subject: "Science",
-            Localtion: "Oxford",
-            price: 100,
-            Stock: 5,
-            image: "science icon.png"
-        },
-
-        {
-            id: 4,
-            Subject: "Histoy",
-            Localtion: "York",
-            price: 80,
-            Stock: 5,
-            image: "history icon.png"
+            "id": 1,
+            "Subject": "Math",
+            "Location": "London",
+            "price": 100,
+            "Space": 5,
+            "image": "math icon.png"
         },
         {
-            id: 5,
-            Subject: "Music",
-            Localtion: "Brisol",
-            price: 90,
-            Stock: 5,
-            image: "Music icon.png"
+            "id": 2,
+            "Subject": "Engilsh",
+            "Location": "London",
+            "price": 100,
+            "Space": 5,
+            "image": "English icon.png"
         },
         {
-            id: 6,
-            Subject: "Math",
-            Localtion: "Manchester",
-            price: 150,
-            Stock: 5,
-            image: "math icon.png"
-        },
-
-        {
-            id: 7,
-            Subject: "Engilsh",
-            Localtion: "Southampton",
-            price: 120,
-            Stock: 5,
-            image: "English icon.png"
-        },
-
-        {
-            id: 8,
-            Subject: "Science",
-            Localtion: "Glasgow",
-            price: 50,
-            Stock: 5,
-            image: "science icon.png"
-        },
-
-        {
-            id: 9,
-            Subject: "Histoy",
-            Localtion: "Newcastle ",
-            price: 140,
-            Stock: 5,
-            image: "history icon.png"
+            "id": 3,
+            "Subject": "Science",
+            "Location": "Oxford",
+            "price": 100,
+            "Space": 5,
+            "image": "science icon.png"
         },
         {
-            id: 10,
-            Subject: "Music",
-            Localtion: "Plymouth",
-            price: 160,
-            Stock: 5,
-            image: "Music icon.png"
+            "id": 4,
+            "Subject": "Histoy",
+            "Location": "York",
+            "price": 80,
+            "Space": 5,
+            "image": "history icon.png"
+        },
+        {
+            "id": 5,
+            "Subject": "Music",
+            "Location": "Brisol",
+            "price": 90,
+            "Space": 5,
+            "image": "Music icon.png"
+        },
+        {
+            "id": 6,
+            "Subject": "Math",
+            "Location": "Manchester",
+            "price": 150,
+            "Space": 5,
+            "image": "math icon.png"
+        },
+        {
+            "id": 7,
+            "Subject": "Engilsh",
+            "Location": "Southampton",
+            "price": 120,
+            "Space": 5,
+            "image": "English icon.png"
+        },
+        {
+            "id": 8,
+            "Subject": "Science",
+            "Location": "Glasgow",
+            "price": 50,
+            "Space": 5,
+            "image": "science icon.png"
+        },
+        {
+            "id": 9,
+            "Subject": "Histoy",
+            "Location": "Newcastle ",
+            "price": 140,
+            "Space": 5,
+            "image": "history icon.png"
+        },
+        {
+            "id": 10,
+            "Subject": "Music",
+            "Location": "Plymouth",
+            "price": 160,
+            "Space": 5,
+            "image": "Music icon.png"
         }
-]
+    ]
